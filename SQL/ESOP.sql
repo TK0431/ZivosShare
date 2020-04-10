f04b7f4b042d48d10904afa59e037f142c61207f
TK0431 <644353584@qq.com>
2020-04-10 18:17:17 +0800

    20200410_5

--- SQL/ESOP.sql
+++ SQL/ESOP.sql
@@ -5,4 +5,16 @@ SELECT A.LineCode,C.LineName,B.FileName,B.FileNO,B.FileMD5,B.FileContent FROM PL
                     LEFT JOIN MDM_Line C ON A.LineCode = C.LineCode
                     WHERE A.Delflag  = 0 AND A.State = 3
 
-select * from MDM_StepProcR
+SELECT A.LineCode,C.LineName,B.FileName,B.FileNO,B.FileMD5,B.FileContent FROM PLAN_WeekPlanWo A
+                    LEFT JOIN MDM_StepProcR B ON A.LineCode = B.Line AND A.BopVersion = B.FlowCode
+                    LEFT JOIN MDM_Line C ON A.LineCode = C.LineCode
+                    WHERE A.Delflag  = 0 AND A.State = 3
+
+SELECT A.LineCode,C.LineName,B.FileName,B.FileNO,B.FileMD5,B.FileContent FROM PLAN_WeekPlanWo A                     
+LEFT JOIN MDM_StepProcR B ON A.LineCode = B.Line AND A.BopVersion = B.FlowCode                    
+ LEFT JOIN MDM_Line C ON A.LineCode = C.LineCode                     
+ WHERE A.Delflag  = 0 AND A.State = 3                                           
+ AND   A.LineCode IN ('XTX','ZZX','TYTX','WCX','TZX','ZJX','BTX','GCX','QXX','KTX','HSX','QGX','XFLX','SFLX','QZX','SGX')
+
+
+ select * from MDM_StepProcR
